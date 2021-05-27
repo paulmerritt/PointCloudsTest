1e95dddc31dd07892d59c792ff810cae6d56aa9e
Paul Merritt <pmerritt@pscr.lz>
2021-05-27 14:05:26 -0600

    adding another test

---
+++ NEONDSSample_converted/cloud.js
@@ -0,0 +1,91 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 6609829,
+    "boundingBox": {
+        "lx": 256000.0,
+        "ly": 4111000.0,
+        "lz": 384.66,
+        "ux": 256999.99000000023,
+        "uy": 4111999.99,
+        "uz": 1384.6500000002236
+    },
+    "tightBoundingBox": {
+        "lx": 256000.0,
+        "ly": 4111000.0,
+        "lz": 384.66,
+        "ux": 256999.99000000003,
+        "uy": 4111999.99,
+        "uz": 510.25
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "gps-time",
+            "size": 8,
+            "elements": 1,
+            "elementSize": 8,
+            "type": "double",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        },
+        {
+            "name": "number of returns",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "source id",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 8.660167694091797,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
