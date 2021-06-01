5b005bc721b1aba2d6962865bdf17a46fd8d58f1
Paul Merritt <pmerritt@pscr.lz>
2021-06-01 13:54:57 -0600

    trying with pre1.7

---
+++ joe/cloud.js
@@ -0,0 +1,51 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 10872461,
+    "boundingBox": {
+        "lx": -12.885109901428223,
+        "ly": -10.261638641357422,
+        "lz": -2.3560304641723635,
+        "ux": 2.8270397186279299,
+        "uy": 5.4505109786987309,
+        "uz": 13.356119155883789
+    },
+    "tightBoundingBox": {
+        "lx": -12.885100000000002,
+        "ly": -10.2616,
+        "lz": -2.3560000000000005,
+        "ux": 1.5018,
+        "uy": 5.4505,
+        "uz": 2.1788000000000005
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "intensity",
+            "size": 2,
+            "elements": 1,
+            "elementSize": 2,
+            "type": "uint16",
+            "description": ""
+        }
+    ],
+    "spacing": 0.13607120513916017,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
